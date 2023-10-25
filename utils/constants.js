b3fa4ae0b08bb5d3166949f460498d96ec35a17a
IMLUOAI <imluoai@gmail.com>
2023-10-25 00:42:39 -0500

    initial commit of untracked files

---
+++ utils/constants.js
@@ -0,0 +1,71 @@
+export const items = [
+  {
+    image:
+      "https://cdn2.vapetasia.com/img/Disposables/Killer_3500_Disposables_Display_Cherry_Peachade_50mg.jpg",
+    title: "Cherry Peach ADE",
+    description:
+      "Quench your thirst with the sweetness of ripened peach stirred into a cup of iced lemonade with plump cherries splashed into every puff!",
+    price: "$100",
+  },
+  {
+    image:
+      "https://cdn2.vapetasia.com/img/Disposables/Killer_3500_Disposables_Display_Blue_Razz_50mg.jpg",
+    title: "Blue Razz",
+    description:
+      "Boxhot presents Blue Razz Shocker, a ready-to-mix drink powder featuring 10 mg of THC per package and an onslaught of in-your-face, sour blue raspberry flavour. The rapid-dissolving powder gives you a consistent dose every time.",
+    price: "$80",
+  },
+  {
+    image:
+      "https://cdn2.vapetasia.com/img/Disposables/Killer_3500_Disposables_Display_Straw_Guaw_50mg.jpg",
+    title: "Straw Guaw",
+    description:
+      "Discover Straw Guaw by Vapetasia Synthetic, featuring delicious strawberry and tropical guava mixed into synthetic nicotine that will delight the senses. Note: This Flavor is now formulated with Tobacco-Free Nicotine, synthetic nicotine not derived from tobacco leaves.",
+    price: "$88",
+  },
+  {
+    image:
+      "https://cdn2.vapetasia.com/img/Disposables/Killer_3500_Disposables_Display_Blueberry_Ice_50mg.jpg",
+    title: "BlueBerry Ice",
+    description:
+      "Blueberry Ice by VaporLax is a fusion of sweet & tart blueberry flavors, tied together with icy menthol notes. A perfect summertime blend.",
+    price: "$88",
+  },
+  {
+    image:
+      "https://cdn2.vapetasia.com/img/Disposables/Killer_3500_Disposables_Display_Grape_Aloe_50mg.jpg",
+    title: "Grape Aloe",
+    description:
+      "Aloe Grape offers a refreshing vaping experience with the delicious flavor of ripe grapes and a cool menthol finish. This combination makes it a great choice for those who enjoy a fruity and invigorating vape.",
+    price: "$90",
+  },
+  {
+    image:
+      "https://cdn2.vapetasia.com/img/Disposables/Killer_3500_Disposables_Display_Trapple_50mg.jpg",
+    title: "Trapple",
+    description:
+      "Shop Trapple by Vapetasia Synthetic, featuring a triple apple mixture, incorporating synthetic nicotine to deliver a fruity vape that delight the senses. Note: This Flavor is now formulated with Tobacco-Free Nicotine, synthetic nicotine not derived from tobacco leaves.",
+    price: "$90",
+  },
+  {
+    image:
+      "https://cdn2.vapetasia.com/img/Disposables/Killer_3500_Disposables_Display_Nana_Berry_50mg.jpg",
+    title: "Nanaberry",
+    description:
+      "The Nana berry bears clusters of small, yellowish green flowers at the end of the branches from September to November. Male and female flowers of this species are borne on different plants. After flowering, shiny bright red fruits are borne in heavy clusters on the female plants from November to January.",
+    price: "$100",
+  },
+];
+
+export const cardListSelector = ".card-list__items";
+export const cardList = document.querySelector(".card-list__items");
+export const popupElement = document.querySelector(".popup");
+export const popupCloseButton = document.querySelector(".popup__close");
+export const popupImage = document.querySelector(".popup__image");
+export const popupCaption = document.querySelector(".popup__caption");
+export const defaultCardButton = document.querySelector(
+  ".filter__button_type_grid"
+);
+export const horizontalCardButton = document.querySelector(
+  ".filter__button_type_column"
+);
